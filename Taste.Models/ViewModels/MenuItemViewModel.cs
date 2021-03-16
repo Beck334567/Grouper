25327b2690b817134030d7a435b63e9727ec914c
Beck <sars88967@gmail.com>
2021-03-16 17:42:17 +0800

    add menuItem VM , CRUD

---
+++ Taste.Models/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Grouper.Models.ViewModels
+{
+    public class MenuItemViewModel
+    {
+        public MenuItem MenuItem { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FoodTypeList { get; set; }
+    }
+}
